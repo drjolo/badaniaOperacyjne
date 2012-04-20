51053ea4a98fe029ba9282714eee9958b95a421e
Bartosz Błażewicz <b.blazewicz@gmail.com>
2012-04-20 19:05:00 +0200

    drobne poprawki w importach

--- src/pl/edu/agh/bo/ui/salesman/SalesmanPanel.java
+++ src/pl/edu/agh/bo/ui/salesman/SalesmanPanel.java
@@ -3,9 +3,7 @@ package pl.edu.agh.bo.ui.salesman;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
-import java.awt.geom.Rectangle2D;
 import javax.swing.JPanel;
 
 import pl.edu.agh.bo.cockroach.CockroachSwarm;
